25979b7071e2e35fd39d1866bf3766780daecee9
Carolina-OH <carito.92@gmail.com>
2020-10-19 11:41:18 -0300

    arreglé status loading

--- src/components/EditUsercomp.vue
+++ src/components/EditUsercomp.vue
@@ -10,7 +10,7 @@
         <input type="text" v-model="form.email" :disabled="!edit"><br>
 
         <button @click.prevent="editUser" :disabled="!edit">Editar</button>
-         <img alt="Welcome logo" src="../assets/loading.gif" :disabled="loading">
+         <img alt="Welcome logo" src="../assets/loading.gif" v-if="loading">
     </form>
     </div>
 </template>
@@ -28,7 +28,7 @@ export default {
                 email:"",
             },
             edit: false,
-            loading:true,
+            loading:false,
         }
     },
     computed: {
@@ -40,19 +40,24 @@ export default {
             let user = this.getUser(this.id)
             //  console.log(user)
             
+            
             if (user === undefined) {
                 let resp = await this.fetchIdUser(this.id)
                 user = resp.data()
             }
+            
             this.form.name = user.name
             this.form.email = user.email 
         },
         editUser(){
             let user = this.form
             user.id = this.id
+            this.loading=true
             let response = this.updateUser(user)
             response.then(()=>{
-                alert('Usuario actualizado')
+                
+                console.log('Usuario actualizado')
+                this.loading=false
                 this.$router.push("/usuarios")
             }).catch(error=>{
                 console.log(error)
